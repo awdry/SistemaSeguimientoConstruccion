dec9735dacc0bec71107a95b005673d2b7e25771
Awdry Nolasco <nolascoawdry@gmail.com>
2026-06-12 23:33:21 -0400

    Modelo de clase Obra

---
+++ src/SeguimientoConstruccion.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeguimientoConstruccion.API.Models;
+
+namespace SeguimientoConstruccion.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Obra> Obras { get; set; }
+
+    }
+}
